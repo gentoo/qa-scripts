0500eaa203e255a71cb55e6332bf8d6e6eb3a166
Christoph Mende <angelos@unkreativ.org>
2011-05-06 20:22:15 +0200

    Minor improvements to filesdur-usage.sh Use pushd/popd instead of cd. Don't rely on portdir being in /usr/portage

--- filesdir-usage.sh
+++ filesdir-usage.sh
@@ -3,11 +3,12 @@
 # $1 is a number, N. The top N directories that are consuming space. Defaults to
 # all.
 
-cd /usr/portage/
+pushd $(portageq portdir) >/dev/null
 if [[ -z $1 ]]; then
 	du -h */*/files | sort -nr
 else
 	du -h */*/files | sort -nr | head -n $1
 fi
+popd >/dev/null
 echo
 echo $(emerge --info | grep Timestamp)

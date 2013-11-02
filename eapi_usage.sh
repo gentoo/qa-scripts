020c6e4b79cb0f612c99f8321c484b10d005a16c
Ulrich Müller <ulm@gentoo.org>
2013-11-02 14:03:36 +0100

    Replace pinspect by manual scanning of md5-cache.
    Previous version can be restored when pkgcore will catch up on EAPI 5.

--- eapi_usage.sh
+++ eapi_usage.sh
@@ -3,8 +3,25 @@
 # Boring 'script' that just uses pkgcore's pinspect command. Someday it would be
 # nice to graph this output, or maybe keep some running history?
 
-[[ $(type pinspect 2> /dev/null) ]] || exit 1
+#[[ $(type pinspect 2> /dev/null) ]] || exit 1
+#
+#pinspect eapi_usage /usr/portage
 
-pinspect eapi_usage /usr/portage
+find /usr/portage/metadata/md5-cache -type f -exec awk '
+    BEGINFILE { found=0 }
+    /^EAPI=/ { sub("EAPI=",""); eapi[$1]++; found=1; nextfile }
+    ENDFILE { if (!found) eapi[0]++ }
+    END { for (i in eapi) print i,eapi[i] }
+  ' '{}' '+' | awk '
+    { eapi[$1]+=$2; total+=$2 }
+    END {
+      for (i in eapi) {
+        s=""; for (j=1; j<eapi[i]*50./total+0.5; j++) s=s"#"
+        printf "EAPI %s: %7d ebuilds (%5.02f%%)  %s\n",
+               i, eapi[i], eapi[i]*100.0/total, s
+       }
+       printf "total:  %7d ebuilds\n", total
+    }'
+
 echo
 echo "Date generated: $(date)"

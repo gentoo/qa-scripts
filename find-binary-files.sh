9b2496f2a230e8f29feda04e141ccd574858cb24
Ulrich Müller <ulm@gentoo.org>
2014-02-08 17:35:15 +0100

    New script to find binary files based on file -i magic.
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

---
+++ find-binary-files.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Copyright 2014 Gentoo Foundation
+# Distributed under the terms of the GNU GPL version 2 or later
+# Author: Ulrich Müller <ulm@gentoo.org>
+
+shopt -s extglob
+
+portdir=$(portageq get_repo_path / gentoo)
+cd "${portdir}" || exit 1
+
+find . \( -path ./distfiles -o -path ./local -o -path ./metadata \) -prune \
+    -o ! -type d -exec file -ih '{}' + \
+| while read line; do
+    path=${line%:*}
+    type=${line##*:*( )}
+    case ${type} in
+        "application/octet-stream; charset=unknown" \
+        | "binary; charset=binary")
+            # GNU Info files (or patches to them) can contain the following
+            # control characters that produce false positives:
+            # - 0x1f, followed by LF or FF
+            # - 0x7f (DEL), preceded by "Node:" in the same line
+            # Filter such characters and reiterate
+            line=$(sed -e 's/\x1f\f\?$//;/Node:/s/\x7f//' "${path}" | file -i -)
+            type=${line##*:*( )}
+            ;;
+    esac
+    case ${type} in
+        text/*) ;;                            # text file
+        application/*"; charset=us-ascii") ;;
+        application/*"; charset=utf-8") ;;
+        "image/svg+xml; charset=us-ascii") ;; # SVG image
+        "image/x-xpmi; charset=us-ascii") ;;  # XPM image
+        *) echo "${path#./}: ${type}" ;;
+    esac
+done \
+| sort 

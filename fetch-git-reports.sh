1f34c2671462eca729ab91b10308b399cb072d42
Michał Górny <mgorny@gentoo.org>
2015-09-07 18:01:31 +0200

    fetch-git-reports: Fetch all branches

--- fetch-git-reports.sh
+++ fetch-git-reports.sh
@@ -13,6 +13,7 @@ for repo in gentoo-ci gpyutils repos; do
 	[[ -d ${repo} ]] || git clone --depth=1 "git://anongit.gentoo.org/report/${repo}.git"
 
 	cd "${repo}"
+	git fetch origin '+refs/heads/*:refs/remotes/origin/*'
 	git pull
 	cd - >/dev/null
 done

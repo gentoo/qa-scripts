d3dc416dd1c0faa1f9a72eabbd4d9b063d089d8a
Michał Górny <mgorny@gentoo.org>
2016-02-12 08:55:56 +0100

    fetch-git-reports: Clean up repository before pulling

--- fetch-git-reports.sh
+++ fetch-git-reports.sh
@@ -13,6 +13,8 @@ for repo in gentoo-ci gpyutils repos; do
 	[[ -d ${repo} ]] || git clone --depth=1 "git://anongit.gentoo.org/report/${repo}.git"
 
 	cd "${repo}"
+	git reset --hard
+	git clean -df
 	git fetch origin '+refs/heads/*:refs/remotes/origin/*'
 	git pull
 	cd - >/dev/null

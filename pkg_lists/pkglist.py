6d5e8c9cd785bceaa6b4f1c6e49399a72a73b956
Brian Evans <grknight@gentoo.org>
2018-07-17 11:26:20 -0400

    pkglist.py: Have print statement work in both Python 2 and 3

--- pkg_lists/pkglist.py
+++ pkg_lists/pkglist.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# $Id: pkglist.py,v 1.1 2005/11/24 23:45:54 ramereth Exp $
 
 import portage
 
@@ -8,13 +7,11 @@ bestpkgs={}
 mycount=0
 
 for x in allpkgs:
-	if x not in bestpkgs.keys():
+	if x not in list(bestpkgs.keys()):
 		y=portage.db["/"]["porttree"].dbapi.xmatch("bestmatch-visible", x)
 		bestpkgs[x]=y
 		if y=="":
 			continue
 		xs=portage.pkgsplit(bestpkgs[x])
-		print xs[0].split("/")[1],
-		print xs[1],
-		print bestpkgs[x]
+		print('%s %s %s' % (xs[0].split("/")[1], xs[1], bestpkgs[x]))
 		mycount+=1

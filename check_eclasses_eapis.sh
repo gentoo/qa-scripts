gentoo/qa-scripts

commit 1fa7be95b899bc1d90d868f6864ee65be798063e

check_eclasses_eapis.sh: Add sanity check for metadata files.
Require a hyphen followed by a digit in their name, which must occur
in a valid ${P} name. Otherwise the find will also pick up Manifest*
files and recognize them as EAPI 0.

Note that we cannot exclude Manifest* explicitly because it is a valid
package name.

Signed-off-by: Ulrich Müller <ulm@gentoo.org>

check_eclasses_eapis.sh
@@ -19,7 +19,7 @@ ECLASSES=$(echo *.eclass)
 popd > /dev/null
 
 #pquery --attr eapi --attr inherited --raw --all --repo portdir > "${TMPEAPIS}"
-find "${REPO_PATH}/metadata/md5-cache" -type f -exec awk -F= '
+find "${REPO_PATH}/metadata/md5-cache" -type f -name '*-[0-9]' -exec awk -F= '
 	BEGINFILE {
 		n = split(FILENAME, f, "/")
 		file = f[n-1] "/" f[n]

commit baa5838bf6667c9ddbdc3eb27219d7bfa163d8a0

check_eclasses_eapis.sh: Typo.
Fixes: 1fa7be95b899bc1d90d868f6864ee65be798063e
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

check_eclasses_eapis.sh
@@ -19,7 +19,7 @@ ECLASSES=$(echo *.eclass)
 popd > /dev/null
 
 #pquery --attr eapi --attr inherited --raw --all --repo portdir > "${TMPEAPIS}"
-find "${REPO_PATH}/metadata/md5-cache" -type f -name '*-[0-9]' -exec awk -F= '
+find "${REPO_PATH}/metadata/md5-cache" -type f -name '*-[0-9]*' -exec awk -F= '
 	BEGINFILE {
 		n = split(FILENAME, f, "/")
 		file = f[n-1] "/" f[n]